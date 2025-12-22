0de49984ff2200d80a1ea1c69e591ba04f6f119d
Steven Wilson <2165238+mendeni@users.noreply.github.com>
2025-12-21 21:46:24 -0500

    More fixes

--- testcmake.sh
+++ testcmake.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -x
 
-echo ####################################################
+echo 8888888888888888888888888888888888888888888888888888888888888888
 
 mkdir build
 cd build
 cmake ..
-cmake -G "Visual Studio 16 2019" -A "x64" -B build
+cmake -G "Visual Studio 17 2022" -A "x64" -B build
 
 pwd
 ls -l 
