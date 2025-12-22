39cd60d6fbadc09c781539e449da740493349461
Steven Wilson <2165238+mendeni@users.noreply.github.com>
2025-12-21 22:24:28 -0500

    More fixes

--- testcmake.sh
+++ testcmake.sh
@@ -2,10 +2,12 @@
 
 echo 8888888888888888888888888888888888888888888888888888888888888888
 
+pw
+
 # mkdir build
 cd build
 cmake ..
 cmake -G "Visual Studio 17 2022" -A "x64" -B build
 
 pwd
-ls -l 
+find . -type f
