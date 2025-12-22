842e5f0c8df00025891a4ca23fe50de093577781
Steven Wilson <2165238+mendeni@users.noreply.github.com>
2025-12-21 22:40:03 -0500

    Fix me

--- testcmake.sh
+++ testcmake.sh
@@ -14,5 +14,6 @@ echo 8888888888888888888888888888888888888888888888888888888888888888
 mkdir build
 cp -v CMakeLists.txt build
 cd build
-cmake ..
+# cmake ..
 cmake -G "Visual Studio 17 2022" -A "x64" -B ..
+find . -type f
